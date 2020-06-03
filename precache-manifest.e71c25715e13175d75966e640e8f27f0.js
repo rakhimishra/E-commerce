f336965338217605cb09f09dceba1e85953514de
rakhimishra <mishrarakhi21@gmail.com>
2020-06-03 12:17:13 +0530

    Updates

---
+++ precache-manifest.e71c25715e13175d75966e640e8f27f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32dbc6aabaf054a3aaddaf6f3f93bbc2",
+    "url": "/rakhimishra/E-commerce.git/index.html"
+  },
+  {
+    "revision": "28f15998cf36a5473d21",
+    "url": "/rakhimishra/E-commerce.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb71fbd38bd792fab327",
+    "url": "/rakhimishra/E-commerce.git/static/css/main.1b9aca8d.chunk.css"
+  },
+  {
+    "revision": "28f15998cf36a5473d21",
+    "url": "/rakhimishra/E-commerce.git/static/js/2.4183be71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rakhimishra/E-commerce.git/static/js/2.4183be71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb71fbd38bd792fab327",
+    "url": "/rakhimishra/E-commerce.git/static/js/main.2f405d93.chunk.js"
+  },
+  {
+    "revision": "eef4fffef5c2f4a7b160",
+    "url": "/rakhimishra/E-commerce.git/static/js/runtime-main.3aafa771.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/rakhimishra/E-commerce.git/static/media/logo.9f7e506d.svg"
+  }
+]);
